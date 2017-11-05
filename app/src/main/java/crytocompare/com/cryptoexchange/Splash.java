68be3390a518f0653ea95b21ddb8ca9d3b8425a3
5ran6 <5raan6@gmail.com>
2017-11-05 13:54:52 +0100

    Added splash Screen

--- app/src/main/java/crytocompare/com/cryptoexchange/Splash.java
+++ app/src/main/java/crytocompare/com/cryptoexchange/Splash.java
@@ -1,13 +1,53 @@
 package crytocompare.com.cryptoexchange;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.transition.Transition;
+import android.support.transition.TransitionValues;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 
 public class Splash extends AppCompatActivity {
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        img = (ImageView) findViewById(R.id.img);
+        final Animation animation = new AlphaAnimation(1, 0); // Change alpha from fully visible to invisible
+        final Transition transition = new Transition() {
+            @Override
+            public void captureEndValues(@NonNull TransitionValues transitionValues) {
+
+            }
+
+            @Override
+            public void captureStartValues(@NonNull TransitionValues transitionValues) {
+
+            }
+        };
+        animation.setDuration(3000); // duration - 3 seconds
+        animation.setInterpolator(new LinearInterpolator()); // do not alter animation rate
+        animation.setRepeatCount(Animation.INFINITE); // Repeat animation infinitely
+        animation.setRepeatMode(Animation.REVERSE); // Reverse animation at the end so the button will fade back in
+        img.startAnimation(animation);
+
+        Handler handler1 = new Handler();
+        handler1.postDelayed(new Runnable() {
+            public void run() {
+                // intent
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000); // 4000 milliseconds delay
+
     }
 }
